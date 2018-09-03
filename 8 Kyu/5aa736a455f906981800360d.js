d3fa6d871db3afeb1ad5d825a5bd2e8a31d5c700
Sabrina Tjeng <sabriele@users.noreply.github.com>
2018-09-03 19:16:39 +0800

    Completed 01/09/2018

---
+++ 8 Kyu/5aa736a455f906981800360d.js
@@ -0,0 +1,8 @@
+/* The Feast of Many Beasts (8 Kyu) */
+
+function feast(beast, dish) {
+	return beast.charAt(0) === dish.charAt(0) &&
+		beast.charAt(beast.length - 1) === dish.charAt(dish.length - 1)
+		? true
+		: false;
+}
